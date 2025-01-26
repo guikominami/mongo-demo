39e23aa0fb16238a77ae58fd92929dc7e262c6ae
guikominami <contato.mudalab@gmail.com>
2025-01-26 22:41:01 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://mongodb:password@localhost:27017/playground")
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.log("Could not connect to MongoDB..."));
+  .catch((err) => console.log("Could not connect to MongoDB...", err.message));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Node.js Course",
+    author: "Mosh",
+    tags: ["node", "backend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
